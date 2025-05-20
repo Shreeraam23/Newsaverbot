2692d49fa8aa022c02cff176ef50a8ba2a069d65
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-05-20 11:24:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ MONGO_DB = getenv("MONGO_DB", "mongodb+srv://koxiher824:uhWjdKAzmfAHcagy@cluster
 LOG_GROUP = getenv("LOG_GROUP", "-1002286606634")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002314245462"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
